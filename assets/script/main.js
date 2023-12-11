d6e818bcfc95a0c3cacbccee2707bf1761dc661b
Jason <jay.jpy.wong@gmail.com>
2023-12-11 10:43:24 +0100

    Button1
    Double click, change background.

--- assets/script/main.js
+++ assets/script/main.js
@@ -6,3 +6,13 @@ interaction.addEventListener('animationend', jumpHandler)
 function jumpHandler() {
   interaction.classList.toggle('jump')
 }
+
+// Button 1 (Double Click Event)
+
+let frontend = document.querySelector('#first')
+
+frontend.addEventListener("dblclick", function() {
+  frontend.classList.add("changeColor");
+} )
+
+// Button 2 
