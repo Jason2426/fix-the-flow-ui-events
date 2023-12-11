93b0614c7076173ed13143ea4c8b45ad6a774eba
Jason <jay.jpy.wong@gmail.com>
2023-12-11 11:31:59 +0100

    Button 3
    Button 3, change width with MouseOver

--- assets/script/main.js
+++ assets/script/main.js
@@ -11,16 +11,29 @@ function jumpHandler() {
 
 let frontend = document.querySelector('#first')
 
-frontend.addEventListener("dblclick", function() {
+frontend.addEventListener("dblclick", function () {
   frontend.classList.add("changeColor");
-} )
+})
 
 // Button 2 
 
 let design = document.querySelector('.second')
 
-design.addEventListener("focusin", function(){
-    this.classList.add("changeColor");
-} )
+design.addEventListener("focusin", function () {
+  this.classList.add("changeColor");
+})
+
+// Button 3 
+
+let and = document.querySelector('a[href="#and"]')
+
+and.addEventListener("mouseover", function () {
+  this.classList.add("changeWidth");
+})
+
+
+
+
+
 
 
